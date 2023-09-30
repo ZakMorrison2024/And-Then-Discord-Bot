acdd093e40d6459d87435d6cb169589189f0ec20
ZakMorrison2024 <146549624+ZakMorrison2024@users.noreply.github.com>
2023-09-30 14:17:40 +0100

    And_Then_Bot_.py

---
+++ And_Then_Bot_.py
@@ -0,0 +1,18 @@
+# And Then
+import discord #import discord libarires
+
+client = discord.Client() #discord client
+
+#client event on message
+@client.event 
+async def on_message(message): # when client types a message
+  if message:
+         text = "And Then ..." # define text
+         await message.channel.send(text) # discord sends data back
+
+async def on_message(message): # when client types a message
+  if message.content.startswith('And Then'):
+         text = "And Then, And Then, And Then!" # define text
+         await message.channel.send(text) # discord sends data back
+
+client.run() # discord run client
